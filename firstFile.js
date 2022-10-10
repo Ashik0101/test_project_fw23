a4796428c215e7468f67d07594768c92eaf4d709
Ashik0101 <115465868+Ashik0101@users.noreply.github.com>
2022-10-10 23:25:50 +0530

    Update firstFile.js

--- firstFile.js
+++ firstFile.js
@@ -1,8 +1,8 @@
 // first code
 // checking odd and even;
 let num= 10;
-if(num % 3==0){
-    console.log("this is even");
+if(num % 2==0){
+    console.log("this is even number");
 }else{
-    console.log("this is odd");
+    console.log("this is odd number");
 }
